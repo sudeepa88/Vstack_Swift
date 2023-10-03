67052126b1d215dfa6df813a5067923523042220
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-04 00:04:35 +0530

    Learned about Vstack

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack{
+            Text("Hello, world!")
+            Button("Button"){
+                
+            }
+            }.background(Color(red: 0.2, green: 0.8, blue: 0.6)).cornerRadius(8)
+            
+            VStack{
+                Text("Sudeepa Come here!").padding([.horizontal, .top],9)
+                Button("Click Me!"){
+                    
+                }
+            }.background(Color.yellow).cornerRadius(8).padding(.top)
+        }
+        
+        
+        
+    }
+    
+}
+
+
+struct AdivView:View{
+    var body: some View{
+        VStack{
+            Text("Sudeepa Come here!")
+            Button("Click Me!"){
+                
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
